df45a187fe06d15a9b359426848606555b25ab29
Gustavo Ariel Molina Segura <gustavo@MacBook-Pro-de-Gustavo.local>
2024-07-01 22:12:02 -0300

    • Se suben cambios al async controller

--- src/async/async.controller.ts
+++ src/async/async.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { AsyncService } from './async.service';
 
 @Controller('async')
-export class AsyncController {}
+export class AsyncController {
+  constructor(private readonly asyncService: AsyncService) {}
+
+  @Post('request')
+  @HttpCode(202)
+  async handleRequest(@Body() body: any): Promise<{ status: string; location: string }> {
+    const requestId = await this.asyncService.startProcess(body.data);
+    return {
+      status: 'Accepted',
+      location: `/async/status/${requestId}`,
+    };
+  }
+}
